fff4b7295828a73ee1f72e19ab2efb62b785c905
Owner <Owner@LAPTOP-SS847KQQ.lan>
2025-02-14 20:43:58 +0900

    Curriculum1.3

---
+++ HelloWorld_FamilynameFirstname/src/sample/Sample.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,World");// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        // 氏名
+		String name = "渡邉朱菜";
+		// 年齢
+		int age = 23;
+		// 趣味
+		String hobby = "スノボー";
+		
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
