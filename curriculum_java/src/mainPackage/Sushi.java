54b54be451b7f3d4facc2f0ad386e8ba2c470be0
Owner <Owner@LAPTOP-SS847KQQ>
2025-04-13 09:52:52 +0900

    java1-22修正

---
+++ curriculum_java/src/mainPackage/Sushi.java
@@ -0,0 +1,30 @@
+package mainPackage;
+
+import logicPackage.JapaneseFood;
+
+/**
+
+*下記がコンソールに出力されるように作成してください
+*
+*	こんにちは！ここは日本です！
+*	この寿司はうまい
+*	寿司は和食です
+*	今の現在日時は2023/03/09 10:23:39です
+*
+*【条件】
+*	・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+*	・適切なファイルにフィールドで変数を必要な数作ってください。
+*	・thisを使って作成してください。
+*	・日時は今日の日付を取得してください。
+*	
+ */
+
+public class Sushi {
+
+	public static void main(String[] args) {
+
+		// SushiDetailsクラスをインスタンス化して情報を表示
+		JapaneseFood jp = new JapaneseFood();
+		jp.displayDetails();
+	}
+}
