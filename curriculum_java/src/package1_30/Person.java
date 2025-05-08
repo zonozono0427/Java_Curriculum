89f55d7e5eec4645ca3d1bd07bdfe94a7a24dbfe
Owner <Owner@LAPTOP-SS847KQQ>
2025-05-08 17:38:29 +0900

    訂正

--- curriculum_java/src/package1_30/Person.java
+++ curriculum_java/src/package1_30/Person.java
@@ -1,46 +1,48 @@
 package package1_30;
 
-
-
 //Personクラスの定義
 class Person {
- // インスタンスフィールド
- String name;
- int age;
- double height;
- double weight;
+	// インスタンスフィールド
+	String name;
+	int age;
+	double height;
+	double weight;
 
- // クラス変数（人数をカウント）
- private static int count = 0;
+	// クラス変数（人数をカウント）
+	private static int count = 0;
 
- // コンストラクタ
- public Person(String name, int age, double height, double weight) {
-     this.name = name;
-     this.age = age;
-     this.height = height;
-     this.weight = weight;
-     count++; // インスタンス生成ごとにカウントアップ
- }
+	// コンストラクタ
+	public Person(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		count++; // インスタンス生成ごとにカウントアップ
+	}
 
- // BMIを計算するメソッド
- public double bmi() {
-     return weight / (height * height);
- }
+	// BMIを計算するメソッド
+	public double bmi() {
+		// 小数第1位で四捨五入
+		double b = height * height;
+		double c = (Math.round(b));
 
- // 四捨五入したBMIを返すメソッド
- public double roundedBmi() {
-     return Math.round(this.bmi() * 10) / 10.0;
- }
+		return weight / c;
+	}
 
- // 自己紹介を出力するメソッド
- public void print() {
-     System.out.println("名前は " + this.name + " です");
-     System.out.println("年は " + this.age + " 歳です");
-     System.out.println("BMIは " + this.roundedBmi() + " です");
- }
+	// 四捨五入したBMIを返すメソッド
+	public double roundedBmi() {
+		return Math.round(this.bmi() * 10) / 10.0;
+	}
 
- // 人数を返す静的メソッド
- public static int getCount() {
-     return count;
- }
+	// 自己紹介を出力するメソッド
+	public void print() {
+		System.out.println("名前は " + this.name + " です");
+		System.out.println("年は " + this.age + " 歳です");
+		System.out.println("BMIは " + this.roundedBmi() + " です");
+	}
+
+	// 人数を返す静的メソッド
+	public static int getCount() {
+		return count;
+	}
 }
